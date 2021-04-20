99620e25882ba62b21e7f7c3ec40abd3eb0d2d09
Dzendo <ASDzendo@gmail.com>
2021-04-20 11:07:31 +0300

    Ready recycler with Click //Codelab - callback ViewModel <-- Xml Elements clicked CardView + Table Preview Update Moshi

--- app/src/main/java/com/dinadurykina/skylexicon/ui/search/SkySearchFragment.kt
+++ app/src/main/java/com/dinadurykina/skylexicon/ui/search/SkySearchFragment.kt
@@ -1,28 +1,17 @@
 package com.dinadurykina.skylexicon.ui.search
 
 import android.content.Context
-import android.net.Uri.parse
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.dinadurykina.skylexicon.R
 import com.dinadurykina.skylexicon.databinding.FragmentSkySearchBinding
-import com.squareup.picasso.Picasso
-import java.net.URI
-import java.net.URI.*
-
-
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
